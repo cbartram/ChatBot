731aafc30c0e20936637a4c74f590d0a55c562e8
g6vc <christian.bartram@bcbsfl.com>
2017-08-01 16:58:30 -0400

    Updated README

--- src/components/Options/Profile/Profile.js
+++ src/components/Options/Profile/Profile.js
@@ -3,7 +3,6 @@ import './style.css';
 import Avatar from '../../Avatar/Avatar';
 
 export default class Profile extends Component {
-
     render() {
         return (
             <div className="row">
